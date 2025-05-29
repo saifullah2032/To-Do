952021e3c11c660dac8eb67270df87f54d890818
SAIFU(RK) <150003102+saifullah2032@users.noreply.github.com>
2025-05-29 05:29:37 +0000

    1st commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.to_do"
+    namespace = "com.SaifXRayan.to_do"
     compileSdk = 35
     ndkVersion = "27.0.12077973"
 
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.to_do"
+        applicationId = "com.SaifXRayan.to_do"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 34
--- android/app/src/main/kotlin/com/example/to_do/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/to_do/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.to_do
+package com.SaifXRayan.to_do
 
 import io.flutter.embedding.android.FlutterActivity
 
